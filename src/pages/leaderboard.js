4f9bb315ff08c0dbd01771fa8d7a8a5693a273e1
cameron-green-1 <cameron.green@voxelstudio.co.uk>
2022-05-05 18:19:04 +0100

    add leaderboard css

--- src/pages/leaderboard.js
+++ src/pages/leaderboard.js
@@ -1,50 +1,58 @@
 import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
+import Header from '../components/header';
 import Fullscreen from '../components/fullscreen';
 import '../styles/globals.css';
 import '../styles/leaderboard.css';
 
-const Leaderboard = () => {
-  const [names, setNames] = useState([]);
-  const [emails, setEmails] = useState([]);
-  const [times, setTimes] = useState([]);
+const Leaderboard = ({ history }) => {
+  const [leaderboardArr, setLeaderboardArr] = useState([]);
 
   const convertTime = (seconds) => {
     let sec = seconds % 60;
     let min = parseInt(seconds / 60);
     if (sec.toString().length == 1) {
-      // padding
-      sec = '00' + sec;
+      sec = '0' + sec;
     }
     return min + ':' + sec;
   };
 
   useEffect(() => {
-    const names = localStorage.getItem('names');
-    // const emails = localStorage.getItem('emails');
-    const times = localStorage.getItem('times');
-    setNames(JSON.parse(names));
-    // setEmails(JSON.parse(emails));
-    setTimes(JSON.parse(times));
+    const storedNames = JSON.parse(localStorage.getItem('names'));
+    const storedTimes = JSON.parse(localStorage.getItem('times'));
+    let leaderboardArr = [];
+    for (let i = 0; i < storedNames.length; i++) {
+      leaderboardArr.push({
+        name: storedNames[i],
+        time: storedTimes[i],
+      });
+    }
+    leaderboardArr.sort(({ time: a }, { time: b }) => a - b);
+    setLeaderboardArr(leaderboardArr);
   }, []);
+
   return (
     <div className='wrapper'>
       <div className='leaderboard'>
-        <p>Leaderboard</p>
-        <ul>
-          {names && times ? (
-            names.map((name, i) => {
+        <Header />
+        {/* <h1>Leaderboard</h1> */}
+        <ul className='leaderboard-list'>
+          {leaderboardArr.length > 0 ? (
+            leaderboardArr.map((obj, i) => {
               return (
-                <li key={i}>{`name = ${names[i]}, time = ${convertTime(
-                  times[i]
-                )}`}</li>
+                <li key={i}>
+                  <h2>{obj.name}</h2>
+                  <p>{convertTime(obj.time)}</p>
+                </li>
               );
             })
           ) : (
             <li>No times to display</li>
           )}
         </ul>
-        <Link to='/'>Link to Home</Link>
+        <button className='home-btn' onClick={() => history.push('/')}>
+          Home
+        </button>
       </div>
       <Fullscreen />
     </div>
